0866c82d70f9cbc65f462ea34a27ce37064ef7d1
Ali <ganceaali@gmail.com>
2024-11-19 15:28:08 +0200

    Added new exercices

---
+++ HangmanGame/wordslist.py
@@ -0,0 +1,9 @@
+# words for Hangman Game
+
+words = (
+    "dog", "cat", "elephant", "tiger", "lion", "bear", "giraffe", "zebra", "fox", "wolf",
+    "rabbit", "deer", "kangaroo", "panda", "koala", "monkey", "chimpanzee", "gorilla", "antelope", "buffalo",
+    "camel", "cheetah", "dolphin", "shark", "whale", "eagle", "owl", "sparrow", "penguin", "flamingo",
+    "peacock", "parrot", "hippopotamus", "rhinoceros", "crocodile", "alligator", "snake", "lizard", "frog", "toad",
+    "squirrel", "otter", "beaver", "hedgehog", "porcupine", "badger", "bat", "rat", "mouse", "hamster"
+)
