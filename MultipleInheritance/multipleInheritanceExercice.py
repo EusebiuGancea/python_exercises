7320df97dcf983ab00f9cf477ecc75e82a1520eb
Ali <ganceaali@gmail.com>
2024-11-21 23:02:06 +0200

    Add new exercices

---
+++ MultipleInheritance/multipleInheritanceExercice.py
@@ -0,0 +1,34 @@
+
+class Animal:
+    def eat(self):
+        print("This animal is eating")
+    
+    def sleep(self):
+        print("This animal is sleeping")
+
+class Prey(Animal):
+    def flee(self):
+        print("This animal is fleeing")
+
+class Predator(Animal):
+    def hunt(self):
+        print("This animal is hunting")
+
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+rabbit = Rabbit()
+hawk = Hawk()
+fish = Fish()
+
+hawk.hunt()
+fish.hunt()  
+
+rabbit.eat()
+rabbit.sleep()
