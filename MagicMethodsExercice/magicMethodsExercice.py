e94362f8fd1cbb88d474a3d67b95f54f9b38f57e
Ali <ganceaali@gmail.com>
2024-11-25 23:10:22 +0200

    Added new exercices

--- MagicMethodsExercice/magicMethodsExercice.py
+++ MagicMethodsExercice/magicMethodsExercice.py
@@ -15,10 +15,37 @@ class Book:
     def __eq__(self, other):
         return self.title == other.title and self.author == other.author
 
+    def __lt__(self, other):
+        return self.num_pages < other.num_pages
+
+    def __gt__(self, other):
+        return self.num_pages > other.num_pages
+
+    def __add__(self, other):
+        return f"{self.num_pages + other.num_pages} pages"
+    
+    def __contains__(self, keyword):
+        return keyword in self.title or keyword in self.author
+
+    def __getitem__(self, key):
+        if key == "title":
+            return self.title
+        elif key == "author":
+            return self.author
+        elif key == "num_pages":
+            return self.num_pages
+        else:
+            return f"Key {key} was not found"
     
 book1 = Book("The Hobbit", "J.R.R. Tolkien", 310)
 book2 = Book("Harry Potter", "J.K. Rowling", 223)
 book3 = Book("The Lion King", "C.S. Lewis", 172)
 
+print("Lion" in book3)
+
 print(book1)
 print(book1 == book2)
+print(book2 < book3)
+print(book1 + book2)
+
+print(book1['title'])
